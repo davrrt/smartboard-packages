2daf4d98dcddb2b152bf043a06af152439851f50
DESKTOP-0LECNDG\NITRO 5 <davidrakotolahy@gmail.com>
2026-05-25 06:41:50 +0300

    feat(ui): exports publics du package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,4 @@
-export {};
+export { cn } from "./cn";
+export { Icon, iconNames } from "./icon-registry";
+export { Button, type ButtonProps } from "./button";
+export { Sidebar, type SidebarProps, type NavEntry } from "./sidebar";
